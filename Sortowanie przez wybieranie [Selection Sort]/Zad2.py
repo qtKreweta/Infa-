f71aac427c0d6e671473c873ad430e522ca1abce
qtKreweta <alekxanderprokurat@gmail.com>
2025-11-23 19:49:31 +0100

    Add selection sort function for sorting words
    Implement selection sort to sort words by length.

--- Sortowanie przez wybieranie [Selection Sort]/Zad2.py
+++ Sortowanie przez wybieranie [Selection Sort]/Zad2.py
@@ -1,1 +1,19 @@
+n = int(input("Podaj liczbę wyrazów: "))
+words = []
+def selection_sort_by_length(tab):
+    size = len(tab)
+    for i in range(size):
+        pmin = i
+        for j in range(i + 1, size):
+            if len(tab[j]) < len(tab[pmin]):
+                pmin = j
+        tab[i], tab[pmin] = tab[pmin], tab[i]
+
+print("Podaj", n, "wyrazów:")
+for _ in range(n):
+    words.append(input())
+
+selection_sort_by_length(words)
+
+print("Posortowane wyrazy:", words)
 
