de5814fb7106cf0ead5c0fe365641d49c99c3055
qtKreweta <alekxanderprokurat@gmail.com>
2025-11-23 13:07:02 +0100

    Update Zad1.py

--- Sortowanie przez wstawianie [Insertion Sort]/Zad1.py
+++ Sortowanie przez wstawianie [Insertion Sort]/Zad1.py
@@ -1,9 +1,11 @@
+n = int(input("Podaj liczbę elementów: "))
+
 lista = []
 for _ in range(n):
     lista.append(int(input("Podaj liczbę: ")))
-n = int(input("Podaj liczbę elementów: "))
-def sortowanie_przez_wstawianie(lista, n):
-    for i in range(1, n):
+
+def sortowanie_przez_wstawianie(lista):
+    for i in range(1, len(lista)):
         pom = lista[i]
         j = i - 1
         while j >= 0 and lista[j] > pom:
@@ -11,7 +13,6 @@ def sortowanie_przez_wstawianie(lista, n):
             j -= 1
         lista[j + 1] = pom
 
-sortowanie_przez_wstawianie(lista, n)
-
+sortowanie_przez_wstawianie(lista)
 print("Posortowana lista:", lista)
 
