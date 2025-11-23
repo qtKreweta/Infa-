b2c9b15eb6da338175b2f4bf55170f2ab59328ad
qtKreweta <alekxanderprokurat@gmail.com>
2025-11-23 19:48:35 +0100

    Update Zad1.py

--- Sortowanie przez wybieranie [Selection Sort]/Zad1.py
+++ Sortowanie przez wybieranie [Selection Sort]/Zad1.py
@@ -1,1 +1,21 @@
+n = int(input("Podaj liczbę elementów: "))
+tab = []
+def selection_sort_numbers(tab):
+    size = len(tab)
+    for i in range(size):
+        pmin = i
+        for j in range(i + 1, size):
+            if tab[j] < tab[pmin]:
+                pmin = j
+        tab[i], tab[pmin] = tab[pmin], tab[i]
+
+
+print("Podaj", n, "liczb:")
+for _ in range(n):
+    tab.append(int(input()))
+
+selection_sort_numbers(tab)
+
+print("Posortowany ciąg:", tab)
+
 
