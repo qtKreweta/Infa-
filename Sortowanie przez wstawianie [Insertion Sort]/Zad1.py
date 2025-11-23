60dd12695d62bfb41bbb6ac60ba77be3eaccc7b7
qtKreweta <alekxanderprokurat@gmail.com>
2025-11-23 12:33:03 +0100

    Implement insertion sort with user input
    Add insertion sort implementation and user input for list

--- Sortowanie przez wstawianie [Insertion Sort]/Zad1.py
+++ Sortowanie przez wstawianie [Insertion Sort]/Zad1.py
@@ -1,1 +1,17 @@
+lista = []
+for _ in range(n):
+    lista.append(int(input("Podaj liczbę: ")))
+n = int(input("Podaj liczbę elementów: "))
+def sortowanie_przez_wstawianie(lista, n):
+    for i in range(1, n):
+        pom = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > pom:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = pom
+
+sortowanie_przez_wstawianie(lista, n)
+
+print("Posortowana lista:", lista)
 
